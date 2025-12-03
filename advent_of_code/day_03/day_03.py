7fa0b79c628397cbac438fd9e9664e3b432c4b03
Alex Whitehead-Smith <alex.me.smith@gmail.com>
2025-12-03 09:47:40 +0000

    Day 03, part 2

--- advent_of_code/day_03/day_03.py
+++ advent_of_code/day_03/day_03.py
@@ -13,6 +13,19 @@ def maximum_joltage(bank: str) -> int:
     return int(largest_not_last + largest_second_digit)
 
 
+def largest_12_digits(bank: str) -> str:
+    n = len(bank)
+    result = []
+
+    for i, battery in enumerate(bank):
+        while result and len(result) + (n - i) > 12 and result[-1] < battery:
+            result.pop()
+        if len(result) < 12:
+            result.append(battery)
+
+    return "".join(result)
+
+
 if __name__ == "__main__":
     file = Path(__file__).with_name("input.txt")
     with file.open() as f:
@@ -20,3 +33,6 @@ if __name__ == "__main__":
 
     print("Part 1")
     print(sum(maximum_joltage(bank) for bank in banks))
+
+    print("Part 2")
+    print(sum(int(largest_12_digits(bank)) for bank in banks))
--- advent_of_code/day_03/test_day_03.py
+++ advent_of_code/day_03/test_day_03.py
@@ -1,4 +1,4 @@
-from day_03 import maximum_joltage
+from day_03 import largest_12_digits, maximum_joltage
 
 INPUT = [
     "987654321111111",
@@ -8,6 +8,12 @@ INPUT = [
 ]
 
 EXPECTED_JOLTAGE = [98, 89, 78, 92]
+EXPECTED_12_LARGEST = ["987654321111", "811111111119", "434234234278", "888911112111"]
+
+
+def test_largest_12_digits():
+    for input, expected in zip(INPUT, EXPECTED_12_LARGEST):
+        assert largest_12_digits(input) == expected
 
 
 def test_maximum_joltage():
