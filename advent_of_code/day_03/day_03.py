e1c60dc5c88b57ed83cc2dec75475edd58aa1949
Alex Whitehead-Smith <alex.me.smith@gmail.com>
2025-12-03 09:52:18 +0000

    Day 03, refactor

--- advent_of_code/day_03/day_03.py
+++ advent_of_code/day_03/day_03.py
@@ -1,26 +1,14 @@
 from pathlib import Path
 
 
-def maximum_joltage(bank: str) -> int:
-    largest_not_last = max(bank[:-1])
-    search_start = bank.find(largest_not_last)
-
-    largest_second_digit = "0"
-    for battery in bank[(search_start + 1) :]:
-        if battery > largest_second_digit:
-            largest_second_digit = battery
-
-    return int(largest_not_last + largest_second_digit)
-
-
-def largest_12_digits(bank: str) -> str:
-    n = len(bank)
+def largest_n_digits(bank: str, n: int) -> str:
+    bank_length = len(bank)
     result = []
 
     for i, battery in enumerate(bank):
-        while result and len(result) + (n - i) > 12 and result[-1] < battery:
+        while result and len(result) + (bank_length - i) > n and result[-1] < battery:
             result.pop()
-        if len(result) < 12:
+        if len(result) < n:
             result.append(battery)
 
     return "".join(result)
@@ -32,7 +20,7 @@ if __name__ == "__main__":
         banks = [line.strip() for line in f.readlines()]
 
     print("Part 1")
-    print(sum(maximum_joltage(bank) for bank in banks))
+    print(sum(int(largest_n_digits(bank, 2)) for bank in banks))
 
     print("Part 2")
-    print(sum(int(largest_12_digits(bank)) for bank in banks))
+    print(sum(int(largest_n_digits(bank, 12)) for bank in banks))
--- advent_of_code/day_03/test_day_03.py
+++ advent_of_code/day_03/test_day_03.py
@@ -1,4 +1,4 @@
-from day_03 import largest_12_digits, maximum_joltage
+from day_03 import largest_n_digits
 
 INPUT = [
     "987654321111111",
@@ -7,15 +7,15 @@ INPUT = [
     "818181911112111",
 ]
 
-EXPECTED_JOLTAGE = [98, 89, 78, 92]
+EXPECTED_JOLTAGE = ["98", "89", "78", "92"]
 EXPECTED_12_LARGEST = ["987654321111", "811111111119", "434234234278", "888911112111"]
 
 
 def test_largest_12_digits():
     for input, expected in zip(INPUT, EXPECTED_12_LARGEST):
-        assert largest_12_digits(input) == expected
+        assert largest_n_digits(input, 12) == expected
 
 
 def test_maximum_joltage():
     for input, expected in zip(INPUT, EXPECTED_JOLTAGE):
-        assert maximum_joltage(input) == expected
+        assert largest_n_digits(input, 2) == expected
